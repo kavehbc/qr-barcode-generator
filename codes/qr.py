429165835ae45d55ca33478f4c80d158f45779f2
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2025-03-04 13:03:02 -0500

    added styles

--- codes/qr.py
+++ codes/qr.py
@@ -3,11 +3,20 @@ import qrcode
 import io
 import qrcode.image.svg
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers.pil import GappedSquareModuleDrawer
+from qrcode.image.styles.moduledrawers.pil import StyledPilQRModuleDrawer, SquareModuleDrawer, GappedSquareModuleDrawer, CircleModuleDrawer, RoundedModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer
 from utils.data_types import data_types, qr_fields
 from utils.color import hex_to_rgba, make_transparent
 from utils.logo import add_logo
 
+
+qr_styles = {"StyledPilQRModuleDrawer": StyledPilQRModuleDrawer,
+            "SquareModuleDrawer": SquareModuleDrawer,
+            "GappedSquareModuleDrawer": GappedSquareModuleDrawer,
+            "CircleModuleDrawer": CircleModuleDrawer, 
+            "RoundedModuleDrawer": RoundedModuleDrawer,
+            "VerticalBarsDrawer": VerticalBarsDrawer,
+            "HorizontalBarsDrawer": HorizontalBarsDrawer
+}
 
 def main():
     st.title("QR Code")
@@ -23,11 +32,12 @@ def main():
 
     with st.expander("Advanced Parameters"):
         qr_data = st.text_area("QR Data", qr_data_replaced, height=250)
-        col1, col2, col3 = st.columns(3)
+        col1, col2, col3, col4 = st.columns(4)
         qr_size = col1.number_input("Size", value=500, step=1, min_value=50)
         qr_box_size = col2.number_input("Box Size", value=10, step=1, min_value=1, max_value=50)
         qr_border_size = col3.number_input("Border Size", value=1, step=1, min_value=1, max_value=10)
-
+        qr_style_name = col4.selectbox("Drawer Style", options=qr_styles.keys())
+        qr_style = qr_styles_name[qr_style_name]
         col1, col2, col3 = st.columns(3)
         qr_fill_color = col1.color_picker("Fill Color", value='#000000')
         qr_back_color = col2.color_picker("Background Color", value='#FFFFFF')
@@ -46,7 +56,7 @@ def main():
     qr.add_data(qr_data)
     qr.make(fit=True)
     img = qr.make_image(image_factory=StyledPilImage,
-                        module_drawer=GappedSquareModuleDrawer(),
+                        module_drawer=qr_style(),
                         fill_color=qr_fill_color,
                         back_color=qr_back_color).convert('RGBA')
     img = img.resize((qr_size, qr_size))
